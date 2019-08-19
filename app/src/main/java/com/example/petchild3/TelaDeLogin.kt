93047c2b11bbdafaa62d046b07cefa63e8cf6e73
Sharpista <alexandrerobertofilho@gmail.com>
2019-08-19 20:28:24 -0300

    add activites

---
+++ app/src/main/java/com/example/petchild3/TelaDeLogin.kt
@@ -0,0 +1,12 @@
+package com.example.petchild3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaDeLogin : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_de_login)
+    }
+}
