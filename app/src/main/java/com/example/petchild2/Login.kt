aca16dd3a4440c3ff2114287e9b6e52f917f9377
Sharpista <alexandrerobertofilho@gmail.com>
2019-08-20 20:35:26 -0300

    Sprint#1 firts commit

---
+++ app/src/main/java/com/example/petchild2/Login.kt
@@ -0,0 +1,19 @@
+package com.example.petchild2
+
+import androidx.appcompat.app.AppCompatActivity
+
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela__login)
+    }
+    public fun facebookLogin(){
+
+    }
+    public  fun googleLogin(){
+
+    }
+}
