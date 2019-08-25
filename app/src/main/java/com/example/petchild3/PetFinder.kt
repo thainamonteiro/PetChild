d3942929863afc247d1c7fc58345088962b0be3c
Moises <loveloungeoficial@gmail.com>
2019-08-24 21:35:57 -0300

    Adicionando layouts com correcao de bug da image logo da mainActivity tela inicial e inserção dos demais layout

---
+++ app/src/main/java/com/example/petchild3/PetFinder.kt
@@ -0,0 +1,13 @@
+package com.example.petchild3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.petchild3.R
+
+class PetFinder : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_finder)
+    }
+}
---
+++ app/src/main/java/com/example/petchild3/PetFinder_Result.kt
@@ -0,0 +1,12 @@
+package com.example.petchild3
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PetFinder_Result : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pet_finder__result)
+    }
+}
