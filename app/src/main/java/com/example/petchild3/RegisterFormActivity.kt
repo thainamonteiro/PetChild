13d34c8e73937f0cd308dc65e21871e57a5ccc29
Moises <loveloungeoficial@gmail.com>
2019-08-26 19:34:34 -0300

    funcionalidade do botão de cadastro, form de cadastro funcionalidade do botão cancelar

--- app/src/main/java/com/example/petchild3/RegisterFormActivity.kt
+++ app/src/main/java/com/example/petchild3/RegisterFormActivity.kt
@@ -4,36 +4,60 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import kotlinx.android.synthetic.main.activity_register.*
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
 import kotlinx.android.synthetic.main.activity_register_form.*
 
 class RegisterFormActivity : AppCompatActivity(), View.OnClickListener {
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_form)
 
+        // Buttons
         setUpListener()
+
+
+
+
     }
+
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+
+    }
+    // [END on_start_check_user]
 
 
 
     private fun setUpListener() {
 
         btn_Cancelar.setOnClickListener(this)
+        btnEnviarForm.setOnClickListener(this)
     }
 
     override fun onClick(view: View?) {
         when(view){
-            btn_logInTo ->{
+            btnEnviarForm ->{
 
             }
             btn_Cancelar ->{
                 val intent = Intent(this, RegisterActivity::class.java)
                 startActivity(intent)
-
             }
 
         }
     }
+
+
+
+
 }
--- app/src/main/java/com/example/petchild3/TelaDeLogin.kt
+++ app/src/main/java/com/example/petchild3/TelaDeLogin.kt
@@ -1,18 +1,28 @@
 package com.example.petchild3
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_tela__login.*
 
 class TelaDeLogin : AppCompatActivity(), View.OnClickListener {
 
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela__login)
-
         setUpListener()
     }
 
@@ -29,4 +39,5 @@ class TelaDeLogin : AppCompatActivity(), View.OnClickListener {
             }
         }
     }
+
 }
