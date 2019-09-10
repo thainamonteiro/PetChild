e4493157cd4736cd3396aefc2c97861abfbf290c
Carlos Filho <d3_nt@hotmail.com>
2019-09-10 17:09:39 -0300

    Classe Postage

--- app/src/main/java/com/example/petchild3/Model/Postage.kt
+++ app/src/main/java/com/example/petchild3/Model/Postage.kt
@@ -1,5 +1,9 @@
 package com.example.petchild3.Model
 
-class Postage {
-
-}
+data class Postage (
+    val MaxHeight: Int? = null,
+    val MaxWeight: Int? = null,
+    val MinHeight: Int? = null,
+    val MinWeight: Int? = null,
+    val Name: String? = null
+)
